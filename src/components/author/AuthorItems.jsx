79abcb336ac4d04433d52183415ea514a049b5e0
Mason <bevinsm355@gmail.com>
2026-02-26 22:27:16 -0500

    fix/cleaned up code

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -8,18 +8,22 @@ const AuthorItems = () => {
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
+ useEffect(() => {
   async function fetchData() {
     setLoading(true);
-    const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`)
+
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+    );
+
     console.log(data);
     setAuthor(data);
     setData(data.nftCollection);
     setLoading(false);
   }
 
-  useEffect(() =>{
     fetchData();
-  }, [])
+  }, [id]);
 
   return (
     <div className="de_tab_content">
@@ -58,13 +62,13 @@ const AuthorItems = () => {
                         <button>Buy Now</button>
                         <div className="nft__item_share">
                           <h4>Share</h4>
-                          <a href="" target="_blank" rel="noreferrer">
+                          <a href="#" target="_blank" rel="noreferrer">
                             <i className="fa fa-facebook fa-lg"></i>
                           </a>
-                          <a href="" target="_blank" rel="noreferrer">
+                          <a href="#" target="_blank" rel="noreferrer">
                             <i className="fa fa-twitter fa-lg"></i>
                           </a>
-                          <a href="">
+                          <a href="#">
                             <i className="fa fa-envelope fa-lg"></i>
                           </a>
                         </div>
